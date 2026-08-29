ba81a9898039a5cd0ca9e5856ed85365d58dcb55
Ashish Sabar <ashishsabar002@gmail.com>
2026-08-29 11:46:12 +0530

    swapping two numbers using third variable.c
                                              output
    ________________________________________________________________________Enter the first number : 1
    Enter the second number : 2
    Before swapping the values are 1 & 2 
    After swapping the values are 2 & 1

---
+++ Semester-1/PST/C programming /Programs/swapping two numbers using third variable.c
@@ -0,0 +1,19 @@
+Swapping two numbers using third variable
+__________________________________________________________________________________________________________________________________________________________________
+
+#include <stdio.h>
+
+int main() {
+    int num1,num2,temp;
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+    printf("Enter the second number:");
+    scanf("%d",&num2);
+    printf("Before swapping the values were %d & %d\n",num1,num2);
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    printf("After swapping the values are %d & %d",num1,num2);
+
+    return 0;
+}
