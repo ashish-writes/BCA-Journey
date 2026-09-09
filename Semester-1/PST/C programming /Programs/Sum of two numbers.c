c506ddb977aa96cd7505e876351bb53350f174c4
Ashish Sabar <ashishsabar002@gmail.com>
2026-09-09 10:59:07 +0530

    calculate the sum of two numbers

---
+++ Semester-1/PST/C programming /Programs/Sum of two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+ int a  ,b ,sum = 0;
+ printf("Enter the first numbers: ");
+ scanf("%d",&a);
+ printf("Enter the second numbers: ");
+ scanf("%d",&b);
+ sum = a+b;
+ printf("The sum is :%d ",sum);
+ return 0;
+}
