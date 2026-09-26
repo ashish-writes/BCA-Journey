c2889e5c69750dbfc32f3917b0ab6f5f1e32059f
Ashish Sabar <ashishsabar002@gmail.com>
2026-09-26 11:45:59 +0530

    Add area calculation using switch statement
    C program  to calculate the area of a circle ,square ,rectangle ,and triangle using switch statement.

---
+++ Semester-1/PST/C programming /Programs/area_using_switch.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    int choice;
+    float radius, side, length, breadth, base, height, area;
+
+    printf("1. Area of Circle\n");
+    printf("2. Area of Square\n");
+    printf("3. Area of Rectangle\n");
+    printf("4. Area of Triangle\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 1:
+            printf("Enter radius: ");
+            scanf("%f", &radius);
+            area = 3.14 * radius * radius;
+            printf("Area of Circle = %.2f", area);
+            break;
+
+        case 2:
+            printf("Enter side: ");
+            scanf("%f", &side);
+            area = side * side;
+            printf("Area of Square = %.2f", area);
+            break;
+
+        case 3:
+            printf("Enter length: ");
+            scanf("%f", &length);
+            printf("Enter breadth: ");
+            scanf("%f", &breadth);
+            area = length * breadth;
+            printf("Area of Rectangle = %.2f", area);
+            break;
+
+        case 4:
+            printf("Enter base: ");
+            scanf("%f", &base);
+            printf("Enter height: ");
+            scanf("%f", &height);
+            area = 0.5 * base * height;
+            printf("Area of Triangle = %.2f", area);
+            break;
+
+        default:
+            printf("Invalid choice");
+    }
+
+    return 0;
+}
